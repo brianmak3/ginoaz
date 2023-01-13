2fe08323f4e74922d20177e1b1783a3280c4d69a
brianmak3 <brianworkss@gmail.com>
2023-01-13 06:19:29 +0300

    Fix: fetch all products

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,24 +1,39 @@
 const SneaksAPI = require('sneaks-api');
 const Sneaker = require('../models/sneakers');
 const sneaks = new SneaksAPI();
+const _ = require('lodash')
 
 const getSneakersData = () => {
-    console.log("getting Sneakers")
-    sneaks.getProducts("FY2903", 10, function (err, products) {
+    sneaks.getMostPopular(100, (err, products) => {
         if (err)
-            console.log("some ERR")
-        else
-            Sneaker.insertMany([
-                {
-                    code: 'FY2903',
-                    data: products
-                }
-            ], (err, resp) => {
+            console.log("err")
+        else {
+            const newArray = products.map(product => {
+                let {
+                    shoeName, brand, styleID, silhoutte, make,
+                    colorway, retailPrice, thumbnail, releaseDate,
+                    description, imageLinks, urlKey, resellLinks,
+                    goatProductId, resellPrices
+                } = product
+                return {
+                    a: 'c',
+                    shoeName,
+                    styleID,
+                    brand,
+                    silhoutte,
+                    make,
+                    colorway, retailPrice, thumbnail, releaseDate,
+                    description, imageLinks, urlKey, resellLinks,
+                    goatProductId, resellPrices
+                };
+            });
+            Sneaker.insertMany(newArray, (err, resp) => {
                 if (err)
                     throw err
                 else
-                console.log(resp)
+                    console.log(resp)
             })
+        }
     })
 }
 module.exports = {
--- src/models/sneakers.js
+++ src/models/sneakers.js
@@ -1,7 +1,38 @@
 const mongoose = require('mongoose');
 
-const SneakerSchema = new mongoose.Schema({}, { strict: false });
+const SneakerSchema = new mongoose.Schema({
+    lowestResellPrice: {
+        stockX: Number,
+        goat: Number,
+        flightClub: Number
+    },
+    shoeName: String,
+    brand: String,
+    silhoutte: String,
+    styleID: String,
+    make: String,
+    colorway: String,
+    retailPrice: Number,
+    thumbnail: String,
+    releaseDate: String,
+    description: String,
+    imageLinks: [String],
+    urlKey: String,
+    resellLinks: {
+        stockX: String,
+        stadiumGoods: String,
+        goat: String,
+        flightClub: String
+    },
+    goatProductId: Number,
+    resellPrices: {
+        stockX: {},
+        goat: {},
+        stadiumGoods: {},
+        flightClub: {}
+    }
+});
 
-const Sneaker = mongoose.model('Sneaker', SneakerSchema);
+const Sneaker = mongoose.model('sneaker', SneakerSchema);
 
 module.exports = Sneaker;
